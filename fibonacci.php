bfe4fba67d9a7a16a25a053f0021c58fbc3fe88a
Tanaka <102905708+neko385@users.noreply.github.com>
2024-10-16 10:29:12 +0900

    chapter16 fibonacci

--- fibonacci.php
+++ fibonacci.php
@@ -1,17 +1,17 @@
 <?php
 $list = [0, 1];
 
-for($i = 0; $i < 25; ++$i){
+for($i = 0, $j =1; $i < $j; ++$i,++$j){
     
     $list[] = $list[$i] + $list[$i+1];
-    
     
     if ($list[$i] < 10000) {
         echo "{$list[$i]} \n" ;
     } else {
         echo "{$list[$i]} \n" ;
         break;
-    }    
+    }   
+    
 }
 
 
